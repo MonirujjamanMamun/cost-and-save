048b733bed5143680380bb35c1bd708804585b41
Monirujjaman Mamun <monirujjamanmamun2357@gmail.com>
2022-02-17 01:40:19 +0600

    without validation code complited

--- js/app.js
+++ js/app.js
@@ -2,48 +2,69 @@ function convert(id) {
     const input = document.getElementById(id);
     const inputValue = input.value;
     const inputNumber = parseFloat(inputValue);
+
     return inputNumber;
 }
 
-/* function changeText(id, str) {
+function changeText(id, str) {
     const input = document.getElementById(id);
     const value = input.innerText = str;
     return value;
 }
- */
+
+
+
 document.getElementById('expenses-btn').addEventListener('click', function() {
     // income 
     const incomeNumber = convert('income-input');
 
     // expenses
     const foodNumber = convert('food-input');
+
     // rent
     const rentNumber = convert('rent-input');
+
     // clothes
     const clotheNumber = convert('clothes-input');
 
     const expenseTotal = foodNumber + rentNumber + clotheNumber;
+
     // total expenses update 
 
-    // const aa = changeText('total-expense', expenseTotal)
+    changeText('total-expense', expenseTotal)
 
-    const totalExpense = document.getElementById('total-expense');
-    const value = totalExpense.innerText = expenseTotal;
+    /*   const totalExpense = document.getElementById('total-expense');
+      const value = totalExpense.innerText = expenseTotal; */
 
     const balanceTotal = incomeNumber - expenseTotal;
 
-    const balance = document.getElementById('total-balance');
-    balance.innerText = balanceTotal;
-    console.log(balance)
+    changeText('total-balance', balanceTotal)
+        /*  const balance = document.getElementById('total-balance');
+         balance.innerText = balanceTotal; */
+
 
 
     // balance update 
 
+})
+
+document.getElementById('save-btn').addEventListener('click', function() {
+    const mainBalance = convert('income-input');
+
+    const saveInput = convert('saving-input');
+
+    const savingMoney = (mainBalance * saveInput) / 100;
+
+    changeText('saving-amount', savingMoney)
+        /* const savingAmount = document.getElementById('saving-amount');
+        savingAmount.innerText = savingMoney; */
 
 
+    const balance = document.getElementById('total-balance');
+    const currentBalance = balance.innerText;
+    const remainingBalance = currentBalance - savingMoney;
 
-
-
-
-
+    changeText('remaining-balance', remainingBalance)
+        /* const totalBalance = document.getElementById('remaining-balance');
+        totalBalance.innerText = remainingBalance; */
 })
