1eabf51d19d6ef57643a063ab3f755fd878e22a5
Monirujjaman Mamun <monirujjamanmamun2357@gmail.com>
2022-02-17 02:54:24 +0600

    validation complited

--- js/app.js
+++ js/app.js
@@ -15,56 +15,61 @@ function changeText(id, str) {
 
 
 document.getElementById('expenses-btn').addEventListener('click', function() {
+
     // income 
     const incomeNumber = convert('income-input');
 
-    // expenses
-    const foodNumber = convert('food-input');
 
-    // rent
-    const rentNumber = convert('rent-input');
+    // validation 
+    if (incomeNumber < 0) {
+        console.log('Please, give correct number.')
+    } else if (isNaN(incomeNumber)) {
+        console.log('It is not a number.')
 
-    // clothes
-    const clotheNumber = convert('clothes-input');
+    } else {
 
-    const expenseTotal = foodNumber + rentNumber + clotheNumber;
+        // expenses
+        const foodNumber = convert('food-input');
 
-    // total expenses update 
+        // rent
+        const rentNumber = convert('rent-input');
 
-    changeText('total-expense', expenseTotal)
+        // clothes
+        const clotheNumber = convert('clothes-input');
 
-    /*   const totalExpense = document.getElementById('total-expense');
-      const value = totalExpense.innerText = expenseTotal; */
+        // total expense 
+        const expenseTotal = foodNumber + rentNumber + clotheNumber;
 
-    const balanceTotal = incomeNumber - expenseTotal;
+        // total expenses update 
+        changeText('total-expense', expenseTotal)
 
-    changeText('total-balance', balanceTotal)
-        /*  const balance = document.getElementById('total-balance');
-         balance.innerText = balanceTotal; */
+        // total balance 
+        const balanceTotal = incomeNumber - expenseTotal;
 
+        // total balance update
+        changeText('total-balance', balanceTotal)
 
-
-    // balance update 
+    }
 
 })
 
 document.getElementById('save-btn').addEventListener('click', function() {
-    const mainBalance = convert('income-input');
 
+    const mainBalance = convert('income-input');
     const saveInput = convert('saving-input');
 
+    // parcentense calculation
     const savingMoney = (mainBalance * saveInput) / 100;
 
+    // saving Amount update
     changeText('saving-amount', savingMoney)
-        /* const savingAmount = document.getElementById('saving-amount');
-        savingAmount.innerText = savingMoney; */
 
 
     const balance = document.getElementById('total-balance');
     const currentBalance = balance.innerText;
     const remainingBalance = currentBalance - savingMoney;
 
+    // remaining Balance update 
     changeText('remaining-balance', remainingBalance)
-        /* const totalBalance = document.getElementById('remaining-balance');
-        totalBalance.innerText = remainingBalance; */
+
 })
