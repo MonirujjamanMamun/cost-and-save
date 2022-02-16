60bc2cfe39f9bb8f409ecdfd1d4fbb5b503dbbda
Monirujjaman Mamun <monirujjamanmamun2357@gmail.com>
2022-02-17 03:14:50 +0600

    full validation complited

--- js/app.js
+++ js/app.js
@@ -19,23 +19,24 @@ document.getElementById('expenses-btn').addEventListener('click', function() {
     // income 
     const incomeNumber = convert('income-input');
 
+    // expenses
+    const foodNumber = convert('food-input');
+
+    // rent
+    const rentNumber = convert('rent-input');
+
+    // clothes
+    const clotheNumber = convert('clothes-input');
 
     // validation 
-    if (incomeNumber < 0) {
+
+    if (incomeNumber < 0 || foodNumber < 0 || rentNumber < 0 || clotheNumber < 0) {
         console.log('Please, give correct number.')
-    } else if (isNaN(incomeNumber)) {
+
+    } else if (isNaN(incomeNumber) || isNaN(foodNumber) || isNaN(rentNumber) || isNaN(clotheNumber)) {
         console.log('It is not a number.')
 
     } else {
-
-        // expenses
-        const foodNumber = convert('food-input');
-
-        // rent
-        const rentNumber = convert('rent-input');
-
-        // clothes
-        const clotheNumber = convert('clothes-input');
 
         // total expense 
         const expenseTotal = foodNumber + rentNumber + clotheNumber;
@@ -58,18 +59,25 @@ document.getElementById('save-btn').addEventListener('click', function() {
     const mainBalance = convert('income-input');
     const saveInput = convert('saving-input');
 
-    // parcentense calculation
-    const savingMoney = (mainBalance * saveInput) / 100;
+    if (saveInput < 0) {
+        console.log('Please, give correct number.')
 
-    // saving Amount update
-    changeText('saving-amount', savingMoney)
+    } else if (isNaN(saveInput)) {
+        console.log('It is not a number.')
+    } else {
+
+        // parcentense calculation
+        const savingMoney = (mainBalance * saveInput) / 100;
+
+        // saving Amount update
+        changeText('saving-amount', savingMoney)
 
 
-    const balance = document.getElementById('total-balance');
-    const currentBalance = balance.innerText;
-    const remainingBalance = currentBalance - savingMoney;
+        const balance = document.getElementById('total-balance');
+        const currentBalance = balance.innerText;
+        const remainingBalance = currentBalance - savingMoney;
 
-    // remaining Balance update 
-    changeText('remaining-balance', remainingBalance)
-
+        // remaining Balance update 
+        changeText('remaining-balance', remainingBalance)
+    }
 })
