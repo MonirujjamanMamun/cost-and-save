99ff96e312fb60af5538cd3b4bb7fff7397e1700
Monirujjaman Mamun <monirujjamanmamun2357@gmail.com>
2022-02-17 13:17:21 +0600

    taitel change

--- js/app.js
+++ js/app.js
@@ -12,44 +12,32 @@ function changeText(id, str) {
     return value;
 }
 
-
-
 document.getElementById('expenses-btn').addEventListener('click', function() {
-
     // income 
     const incomeNumber = convert('income-input');
-
     // expenses
     const foodNumber = convert('food-input');
-
     // rent
     const rentNumber = convert('rent-input');
-
     // clothes
     const clotheNumber = convert('clothes-input');
     // error Messes 
     const errorMsses = document.getElementById('error-msses');
-
     // validation 
     if (incomeNumber < 0 || foodNumber < 0 || rentNumber < 0 || clotheNumber < 0) {
-
         errorMsses.style.display = 'block'
-
     } else if (isNaN(incomeNumber) || isNaN(foodNumber) || isNaN(rentNumber) || isNaN(clotheNumber)) {
         errorMsses.style.display = 'block'
-
     } else {
-
         // total expense 
         const expenseTotal = foodNumber + rentNumber + clotheNumber;
-
         // total expenses update 
         changeText('total-expense', expenseTotal)
-
+            // error 
         const errorMsses3 = document.getElementById('error-msses3');
+        // validation 
         if (incomeNumber < expenseTotal) {
-
-            errorMsses3.style.display = 'block'
+            errorMsses3.style.display = 'block';
         } else {
             // total balance 
             const balanceTotal = incomeNumber - expenseTotal;
@@ -58,14 +46,8 @@ document.getElementById('expenses-btn').addEventListener('click', function() {
             errorMsses3.style.display = 'none'
         }
 
-
-
-
-
         errorMsses.style.display = 'none'
-
     }
-
 })
 
 document.getElementById('save-btn').addEventListener('click', function() {
@@ -76,17 +58,13 @@ document.getElementById('save-btn').addEventListener('click', function() {
     // validation 
     if (saveInput < 0) {
         errorMsses2.style.display = 'block'
-
     } else if (isNaN(saveInput)) {
         errorMsses2.style.display = 'block'
     } else {
-
         // parcentense calculation
         const savingMoney = (mainBalance * saveInput) / 100;
-
         // saving Amount update
         changeText('saving-amount', savingMoney)
-
 
         const balance = document.getElementById('total-balance');
         const currentBalance = balance.innerText;
